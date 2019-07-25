4ffa09b90e89ea6c207d113dd094d9701e02716e
aya43@sfu.ca <aya43@sfu.ca>
2019-07-24 18:19:44 -0700

    update

---
+++ _run.R
@@ -0,0 +1,19 @@
+root = "/mnt/f/Brinkman group/current/Alice/flowtype_metric"
+setwd(root)
+
+source("source/00_data_bodenmiller.R")
+source("source/00_data_ctrl.R")
+source("source/00_data_flowcap.R")
+source("source/00_data_pos.R")
+source("source/00_data_pregnancy.R")
+source("source/01_feat_normalizeadj.R")
+source("source/01.5_data_flowcap2.R")
+source("source/01.5_data_flowcap3.R")
+source("source/02_meta_cell.R")
+source("source/03_feat_childparent.R")
+# source("source/05_stat_checkmatrix.R")
+source("source/06_pvalv2.R")
+source("source/07_pval_plot.R")
+
+
+
