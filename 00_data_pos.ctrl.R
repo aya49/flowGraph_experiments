2b1c9545adff5f0bb78a84c2244da41da96544cd
aya43@sfu.ca <aya43@sfu.ca>
2019-08-13 22:24:29 -0700

    controls pos

--- 00_data_pos.ctrl.R
+++ 00_data_pos.ctrl.R
@@ -78,7 +78,7 @@ thress4[gthresm[1:4]] = quantile(cvd, .501)
 save.image(paste0(root,"/temp.Rdata"))
 
 # start = Sys.time()
-for (ds in c("ctrl","pos1","pos2","pos3","pos4")) {
+for (ds in c("ctrl1","ctrl2","ctrl3","ctrl4","ctrl5","ctrl6","ctrl7","ctrl8","ctrl9","ctrl10","pos1","pos2","pos3","pos4")) {
   # clear/load memory
   a = ls(all=T); a = a[!a%in%c("ds","root")]
   rm(list=a); gc()
@@ -89,89 +89,113 @@ for (ds in c("ctrl","pos1","pos2","pos3","pos4")) {
   setwd(root)
   registerDoMC(no_cores)
   
-
+  
   # ouput directories
   result_dir = paste0(root, "/result/",ds)
   meta_dir = paste(result_dir, "/meta", sep=""); dir.create(meta_dir, showWarnings=F, recursive=T)
   feat_dir = paste(result_dir, "/feat", sep=""); dir.create(feat_dir, showWarnings=F)
   feat_file_cell_count_dir = paste(feat_dir,"/file-cell-countAdj",sep="")
-
+  
   # flowtype
   ftl = llply(loopInd(1:nsample,no_cores), function(ii) {
     llply(ii, function(i) {
       f@exprs = matrix(rnorm(ncells[i]*length(markers),2,1), nrow=ncells[i])
       thress = thress0
-      if (i>(nsample*nctrl)) {
+      if (i>(nsample*nctrl & !grepl("ctrl",ds))) {
         if (ds=="pos3") {
           # .125 -> .19 a+b+c+
-          f@exprs = matrix(rnorm(ncells[i]*length(markers),2,1), nrow=ncells[i])
-          ft = flowType(Frame=f, PropMarkers=ci, MarkerNames=markers, 
-                        MaxMarkersPerPop=6, PartitionsPerMarker=2, 
-                        Thresholds=thress, 
-                        Methods='Thresholds', verbose=F, MemLimit=60)
-          ftcell = unlist(lapply(ft@PhenoCodes, function(x)
-            decodePhenotype(x, ft@MarkerNames, rep(2,length(ft@MarkerNames))) ))
-          ftv0 = ft@CellFreqs
-          ftv0 = round(ftv0/ftv0[1],3)
+          # f@exprs = matrix(rnorm(ncells[i]*length(markers),2,1), nrow=ncells[i])
+          # ft = flowType(Frame=f, PropMarkers=ci, MarkerNames=markers, 
+          #               MaxMarkersPerPop=4, PartitionsPerMarker=2, 
+          #               Thresholds=thress, 
+          #               Methods='Thresholds', verbose=F, MemLimit=60)
+          # ftcell = unlist(lapply(ft@PhenoCodes, function(x)
+          #   decodePhenotype(x, ft@MarkerNames, rep(2,length(ft@MarkerNames))) ))
+          # ftv0 = ft@CellFreqs
+          # ftv0 = round(ftv0/ftv0[1],3)
           
           ap = f@exprs[,1]>thress[[1]]
           bp = f@exprs[,2]>thress[[2]]
           cp = f@exprs[,3]>thress[[3]]
-          ab = (ap & bp)
-          ac = (ap & cp)
-          bc = (bp & cp)
+          dp = f@exprs[,4]>thress[[4]]
+          # ep = f@exprs[,5]>thress[[5]]
           triple = (ap & bp & cp)
+          # quad = ap & bp & cp & dp
+          # quint = ap & bp & cp & dp & ep
           
-          all = ap | bp | cp
-          non = !all
-          doublea = bc & cp & !ap
-          doubleb = ap & cp & !bp
-          doublec = ap & bp & !cp
-          singlea = ap & !bp & !cp
-          singleb = bp & !ap & !cp
-          singlec = cp & !ab & !bp
           
-          # single = singlea | singleb | singlec
-          # double = (ab | bc | ac) & !triple
           tm = sum(triple)/2
-          f@exprs[sample(which(doublea),tm),1] = p75
-          f@exprs[sample(which(doubleb),tm),1] = p25
-          f@exprs[sample(which(doublec),tm),1] = p25
-          f@exprs[sample(which(non),tm),1] = p75
-          f@exprs[sample(which(doublea),tm),2] = p25
-          f@exprs[sample(which(doubleb),tm),2] = p75
-          f@exprs[sample(which(doublec),tm),2] = p25
-          f@exprs[sample(which(non),tm),2] = p75
-          f@exprs[sample(which(doublea),tm),3] = p25
-          f@exprs[sample(which(doubleb),tm),3] = p25
-          f@exprs[sample(which(doublec),tm),3] = p75
-          f@exprs[sample(which(non),tm),3] = p75
+          f@exprs[sample(which(bp & cp & !ap),tm),1] = p75 # bc
+          f@exprs[sample(which(ap & cp & !bp),tm),1] = p25 # ac
+          f@exprs[sample(which(ap & bp & !cp),tm),1] = p25 # ab
+          f@exprs[sample(which(!ap & !bp & !cp),tm),1] = p75 # a
           
-         ft = flowType(Frame=f, PropMarkers=ci, MarkerNames=markers, 
-                   MaxMarkersPerPop=6, PartitionsPerMarker=2, 
-                   Thresholds=thress, 
-                   Methods='Thresholds', verbose=F, MemLimit=60)
-         ftcell = unlist(lapply(ft@PhenoCodes, function(x)
-           decodePhenotype(x, ft@MarkerNames, rep(2,length(ft@MarkerNames))) ))
-         ftv = ft@CellFreqs
-         ftv = round(ftv/ftv[1],3)
-         names(ftv) = ftcell
-         a = getPhenCP(cp=ftcell,no_cores=no_cores)
-         al = layout_gr(a$gr$e,a$gr$v)
-         alp = layout_gr(a$grp$e,a$grp$v)
-         al = gpdf(al)
-         al$v$label = paste0(al$v$name,":",ftv)
-         gp = gggraph(al, v_ind=rep(F,nrow(al$v)), e_ind=rep(F,nrow(al$e)), label_ind=rep(T,nrow(al$v)))
-         gp = gp + geom_label_repel(
-           data=al$v[(ftv-ftv0)/ftv0 >.1,],
-           aes(x=x,y=y,label=label, color=color), nudge_y = .3)
-         plot(gp)
-
-        } else if (ds!="ctrl") {
+          # tn = sum(quint)/2
+          # f@exprs[sample(which(!ap & bp & cp & dp & ep),tn),1] = p75
+          # f@exprs[sample(which(ap & !bp & cp & dp & ep),tn),1] = p25
+          # f@exprs[sample(which(ap & bp & !cp & dp & ep),tn),1] = p25
+          # f@exprs[sample(which(ap & bp & cp & !dp & ep),tn),1] = p25
+          # f@exprs[sample(which(ap & bp & cp & dp & !ep),tn),1] = p25
+          # 
+          # f@exprs[sample(which(!ap & bp & cp & !dp & !ep),tn),1] = p75
+          # f@exprs[sample(which(!ap & bp & !cp & dp & !ep),tn),1] = p75
+          # f@exprs[sample(which(!ap & bp & !cp & !dp & ep),tn),1] = p75
+          # f@exprs[sample(which(!ap & !bp & cp & dp & !ep),tn),1] = p75
+          # f@exprs[sample(which(!ap & !bp & cp & !dp & ep),tn),1] = p75
+          # f@exprs[sample(which(!ap & !bp & !cp & dp & ep),tn),1] = p75
+          # 
+          # f@exprs[sample(which(ap & bp & !cp & !dp & !ep),tn),1] = p25
+          # f@exprs[sample(which(ap & !bp & cp & !dp & !ep),tn),1] = p25
+          # f@exprs[sample(which(ap & !bp & !cp & dp & !ep),tn),1] = p25
+          # f@exprs[sample(which(ap & !bp & !cp & !dp & ep),tn),1] = p25
+          # 
+          # f@exprs[sample(which(!ap & !bp & !cp & !dp & !ep),tn),1] = p75
+          
+          
+          # ft = flowType(Frame=f, PropMarkers=ci, MarkerNames=markers, 
+          #           MaxMarkersPerPop=4, PartitionsPerMarker=2, 
+          #           Thresholds=thress, 
+          #           Methods='Thresholds', verbose=F, MemLimit=60)
+          # ftcell = unlist(lapply(ft@PhenoCodes, function(x)
+          #   decodePhenotype(x, ft@MarkerNames, rep(2,length(ft@MarkerNames))) ))
+          # ftv = ft@CellFreqs
+          # ftv = round(ftv/ftv[1],3)
+          # names(ftv) = ftcell
+          # a = getPhenCP(cp=ftcell,no_cores=no_cores)
+          # al = layout_gr(a$gr$e,a$gr$v)
+          # alp = layout_gr(a$grp$e,a$grp$v)
+          # al = gpdf(al)
+          # al$v$label = paste0(al$v$name,":",ftv)
+          # gp = gggraph(al, v_ind=rep(F,nrow(al$v)), e_ind=rep(F,nrow(al$e)), label_ind=rep(T,nrow(al$v)))
+          # gp = gp + geom_label_repel(
+          #   data=al$v[str_count(al$v$name,"[-+]")==3 & !grepl("-",al$v$name) & abs(ftv-ftv0)/ftv0 >.1,], #grepl("A[+|-]B[+|-]C[+|-]",al$v$name) 
+          #   aes(x=x,y=y,label=label, color=color), nudge_y = .3)
+          # plot(gp)
+          
+        } else if (ds=="pos4") {
+          
+          ap = f@exprs[,1]>thress[[1]]
+          bp = f@exprs[,2]>thress[[2]]
+          cp = f@exprs[,3]>thress[[3]]
+          dp = f@exprs[,4]>thress[[4]]
+          # ep = f@exprs[,5]>thress[[5]]
+          # triple = (ap & bp & cp)
+          quad = ap & bp & cp & dp
+          # quint = ap & bp & cp & dp & ep
+          
+          tn = sum(quad)/2
+          f@exprs[sample(which(!ap & bp & cp & dp),tn),1] = p75 # bcd
+          f@exprs[sample(which(ap & !bp & cp & dp),tn),1] = p25 # acd
+          f@exprs[sample(which(ap & bp & !cp & dp),tn),1] = p25 # abd
+          f@exprs[sample(which(ap & bp & cp & !dp),tn),1] = p25 # abc
+          f@exprs[sample(which(!ap & bp & !cp & !dp),tn),1] = p75 # ab
+          f@exprs[sample(which(!ap & !bp & cp & !dp),tn),1] = p75 # ac
+          f@exprs[sample(which(!ap & !bp & !cp & dp),tn),1] = p75 # ad
+          f@exprs[sample(which(ap & !bp & !cp & !dp),tn),1] = p25 # a
+        } else  {
           thress = switch(ds, 
                           pos1 = thress1,
-                          pos2 = thress2,
-                          pos4 = thress4)
+                          pos2 = thress2)
         }
       }
       flowType(Frame=f, PropMarkers=ci, MarkerNames=markers, 
@@ -193,7 +217,7 @@ for (ds in c("ctrl","pos1","pos2","pos3","pos4")) {
   ft_ = as.matrix(ft)
   save(ft_, file=paste0(feat_file_cell_count_dir,".Rdata"))
   if (writecsv) write.csv(ft_, file=paste0(feat_file_cell_count_dir,".csv"), row.names=T)
-
+  
   time_output(start2, ds)
 }
 time_output(start)
