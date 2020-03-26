19d225bad2ebbe81c19fb1c891ed16738d008099
Alice Yue <aya43@sfu.ca>
2020-03-25 20:27:36 -0700

    update

---
+++ flowTypeFilter/c_code/flowTypeFilter.h
@@ -0,0 +1,18 @@
+#ifndef _FILTER_H_
+#define _FILTER_H_
+
+#include <Rcpp.h>
+
+SEXP countCells (   //Inputs:
+                        SEXP RPartitionsPerChannel, //no of partitions per channel
+                        SEXP RThresholds,   //threshold points per channel; list of vectors
+                        SEXP RmaxPopSize,
+                        SEXP RthresChannels, // labels of threshold marker channels
+                        SEXP RMfiData, // expr data from the flowFrame, only including mfi channels
+                        SEXP RFrameExprData, //expr data from the flowFrame
+                        SEXP RNumPops, //total number of populations
+                        SEXP Rverbose //total number of populations
+                        );
+
+
+#endif // _FILTER_H_
---
+++ flowTypeFilter/package/flowTypeFilterC.Rcheck/00_pkg_src/flowTypeFilterC/src/flowTypeFilterC.h
@@ -0,0 +1,19 @@
+#ifndef _FILTER_H_
+#define _FILTER_H_
+
+#include <Rcpp.h>
+
+SEXP countCells (   //Inputs:
+                        SEXP RFilter, // true if filter, false if thresholds
+                        SEXP RPartitionsPerChannel, //no of partitions per channel
+                        SEXP RThresholds,   //threshold points per channel; list of vectors
+                        SEXP RmaxPopSize,
+                        SEXP RthresChannels, // labels of threshold marker channels
+                        SEXP RMfiData, // expr data from the flowFrame, only including mfi channels
+                        SEXP RFrameExprData, //expr data from the flowFrame
+                        SEXP RNumPops, //total number of populations
+                        SEXP Rverbose //total number of populations
+                        );
+
+
+#endif // _FILTER_H_
